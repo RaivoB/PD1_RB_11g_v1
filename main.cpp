2c2cb7fe74cef4e9ecbac57e710a8cbe31c5176c
Raivo Bukovskis <rb2814@gymn.local>
2021-11-26 09:54:41 +0200

    meginajums pievienot saskaitisanu

--- main.cpp
+++ main.cpp
@@ -27,18 +27,33 @@ int sus(int x, int y, int r)
     srand((unsigned)time(NULL));
 
     double mas[x][y];
+    int SumR, SumK, SumM;
     int p;
     p = r;
 
-
     for (int i = 0; i < x; i++)
 {
+
 	for (int j = 0; j < y; j++)
 	{
    mas[x][y] = rand() % p;
    cout << mas[x][y] << " ";
+
 	}
 	cout << endl;
+
+   for (int i = 0; i < x; i++)
+{
+
+	for (int j = 0; j < y; j++)
+	{
+   SumR = SumR + mas[i][j];
+
+}
+ cout << SumR << endl;
+}
+	cout << endl;
+
 }
  return y;
 }
